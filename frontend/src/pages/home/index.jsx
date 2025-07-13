757759a36f92c97adb914a88d6ce944d7d8f7f62
Rian Duarte Matos de Oliveira <123524730+riandte@users.noreply.github.com>
2025-07-13 11:21:54 -0300

    primeiro commit, Tela de cadastro criada

---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,20 @@
+import './style.css'
+import Button from 'react-bootstrap/Button';
+
+function Home() {
+
+  return (
+    <div>
+      GymFLOW
+      <Button variant="primary" size="lg" >
+        Large button
+      </Button>
+      <Button variant="secondary" size="lg">
+        Large button
+      </Button>
+      <a href="./cadastrarUsuarios">iutytyu</a>
+    </div>
+  )
+}
+
+export default Home
